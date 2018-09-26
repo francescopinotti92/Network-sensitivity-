6826f02640223950a3b06fe0be8930770cda9a8d
Domenico Di Gangi <digangidomenico@gmail.com>
2018-09-27 00:17:58 +0100

    Communities Code and Figures
    Changed communities_main.m in order to allow for random shuffling of nodes between the two communities. I also uploaded 2 versions of Fig 4 left from arxiv_v1. One with the corrected code and one with the bug

--- Matlab_code/block_structure/communities_main.m
+++ Matlab_code/block_structure/communities_main.m
@@ -3,7 +3,7 @@
 
 % the idea is to sample many realizations of a topological structure with
 % clear communities and realistical strengths distribution taken from real
-% data. After the topology is 
+% data. 
 clc
 close all
 clear
@@ -31,57 +31,69 @@ n_run = 500;
 sr =BankscopeRescaled.LoansandAdvancestoBanksmilUSD2013; % in strengths
 sc =BankscopeRescaled.DepositsfromBanksmilUSD2013; % out strengths
 equityBeforeShock = BankscopeRescaled.EquitymilUSD2013;
-% shuffle the strengths and equities
-[sr,sc,equityBeforeShock] = shuffleStrenghtSequence(sr,sc,equityBeforeShock);
 
-
-%divide the banks in 2 groups of equal size
+N_banksTot = size(equityBeforeShock,1);
+%select the size of the first group to which banks will be assigned
+%randomly
 NbanksGroup1 = 50;
 %Maximum number of iterations for the debt rank function to converge
 max_iter = 10^5;
-%intensity of the shock
-rho=0.1;
-
-N_banksTot = size(equityBeforeShock,1);
+%density of the network
+density=0.1;
 % shock vector such that only banks in one group are shocked
 ShockedBanks = [ones(1, NbanksGroup1), zeros(1, N_banksTot- NbanksGroup1)];
-h=figure()
-hold on
+
+
 tic
 % phi is the magnitude of the shock, for those banks that are shocked
-phiVals = 0:0.05:1;
-
-for lambda = [0.005:0.1:0.3 1] 
+phiVals = [0:0.005:0.1 0.15:0.05:1] ;
+lambdaVlas = [0.005:0.1:0.3 1] ;nLambda = numel(lambdaVlas); 
+storeDebtRank = cell(nLambda,1);
+StackedMatrix = zeros(N_banksTot,N_banksTot,n_run);
+for indLambda = 1:nLambda
+    lambda = lambdaVlas(indLambda);
     dR_vec = zeros(n_run,numel(phiVals));
-    % Density        
-    z11 = invertDensityAsFunctionOfZ_Comm(sr,sc,lambda,rho);
+    % fix the Density for a given lambda value        
+    z11 = invertDensityAsFunctionOfZ_Comm(sr,sc,lambda,density);
     z12 = lambda*z11; z21=z12; z22 = z11;
     zMat = [z11*ones(NbanksGroup1)  z12*ones(NbanksGroup1,N_banksTot- NbanksGroup1) ;
             z21*ones(N_banksTot- NbanksGroup1,NbanksGroup1)  z22*ones(N_banksTot-NbanksGroup1) ];
-
-    StackedMatrix = sampleCimini(sr,sc,zMat,n_run);
+    
     idx_phi = 0;
-    for phi = phiVals
+    for phi = phiVals    
         idx_phi = idx_phi+1;
-        for i = 1:n_run
-            interbankLiabilitiesMatrix = StackedMatrix(:,:,i);
+        % shuffle the strengths and equities, sample the shuffled communities, 
+        % and then run debt rank
+        for n=1:n_run
+            [srSample,scSample,equityBeforeShockSample] = shuffleStrenghtSequence(sr,sc,equityBeforeShock,'evenodd');%'random');
+            % to have the figure from the bugged code use sampleCiminiBug
+            StackedMatrix(:,:,n) = sampleCimini(srSample,scSample,zMat,1);    
+            interbankLiabilitiesMatrix = StackedMatrix(:,:,n);
             shock = ShockedBanks*phi;
-            [~, h_t, ~] = debtrank(interbankLiabilitiesMatrix, equityBeforeShock, shock, max_iter);
-            dR_vec(i, idx_phi) = sum((h_t(~ShockedBanks)-shock(~ShockedBanks)) .* equityBeforeShock(~ShockedBanks)' ./ sum(equityBeforeShock(~ShockedBanks)));
+            [~, h_t, ~] = debtrank(interbankLiabilitiesMatrix, equityBeforeShockSample, shock, max_iter);
+            dR_vec(n, idx_phi) = sum((h_t(~ShockedBanks)-shock(~ShockedBanks)) .* equityBeforeShockSample(~ShockedBanks)' ./ sum(equityBeforeShockSample(~ShockedBanks)));
         end
     end
-    plot(phiVals, mean(dR_vec), 'DisplayName', ['\beta = ', num2str(lambda)])
+    storeDebtRank{indLambda} = dR_vec;
 end
+
 toc
 
-%% Test that the sampled matrices enforce the constraints
+%% take the averages
+tmpPlot = [];
+legTex = cell(nLambda,1);
+for indLambda = 1:nLambda
+    tmpPlot = [tmpPlot; mean(storeDebtRank{indLambda} ,1) ];
+    legTex{indLambda} = ['\lambda = ' num2str(lambdaVlas(indLambda))] ;
+end
 
-
-
-meanMat  = mean(StackedMatrix,3);
-max(abs(sum(meanMat,1)' - sr )./sr)
-max(abs(sum(meanMat,2) - sc )./sc)
-
+%% plot
+close all
+figure()
+plot(phiVals, tmpPlot')
+legend(legTex,'location','best')
+xlabel('\phi')
+ylabel('Debt Rank')
 
 
 
--- Matlab_code/matrixSamplingCode/shuffleStrenghtSequence.m
+++ Matlab_code/matrixSamplingCode/shuffleStrenghtSequence.m
@@ -1,11 +1,20 @@
-function [srOut,scOut, equityOut] = shuffleStrenghtSequence(sr,sc, equity)
-    % shuffle the input vectors dividing it in odd and even components and
-    % then vertically gluing them
-    %this function can be useful when we wish to divide banks into two
-    %groups without a marked difference in the average size of banks in
-    %each group. Banks in the data are almost sorted according to size!!
+function [srOut,scOut, equityOut] = shuffleStrenghtSequence(sr,sc, equity,mode)
+% shuffle the three input vectors but keep the ordering equal in all three
+
+switch upper(mode)
+    case 'EVENODD'
+        % shuffle the input vectors dividing it in odd and even components and
+        % then vertically gluing them
+        srOut = [sr(1:2:end);sr(2:2:end)];
+        scOut = [sc(1:2:end);sc(2:2:end)];
+        equityOut = [equity(1:2:end);equity(2:2:end)];
     
-    srOut = [sr(1:2:end);sr(2:2:end)];
-    scOut = [sc(1:2:end);sc(2:2:end)];
-    equityOut = [equity(1:2:end);equity(2:2:end)];
- end
+    case 'RANDOM'
+        % randomly shuffle
+        newOrd  = randperm(numel(sr))';
+        srOut = sr(newOrd);
+        scOut = sc(newOrd);
+        equityOut = equity(newOrd);
+end
+
+end
